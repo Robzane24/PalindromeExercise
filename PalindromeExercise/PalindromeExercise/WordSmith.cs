9f615bf7c41add2973e90cd3f2d95a67a2412a9d
Robert Zane <rob.zane24@gmail.com>
2022-12-28 09:49:51 -0600

    Completed PalindromeExercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            string reversedword = "";
+
+            for (int i = word.Length-1; i >= 0; i--)
+            {
+                reversedword += word[i];
+            }
+            return (reversedword.ToLower() == word.ToLower());
+            
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,18 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("work", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            var instance = new WordSmith();
 
+            var actual = instance.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
+
+            
         }
     }
 }
